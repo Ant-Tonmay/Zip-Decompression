171105c5b8904d76616ee8d807f925b8f9023f4a
RedAnt-Tonmay <tonmaysardar500@gmail.com>
2023-09-30 00:49:10 +0530

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use std::fs;
+use std::io;
+
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <zip-file>", args[0]);
+        std::process::exit(1);
+    }
+
+    let file_name = &args[1];
+    let file = fs::File::open(&file_name)?;
+    let mut archive = zip::ZipArchive::new(file)?;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+
+        if file.name().ends_with('/') {
+            println!("Directory: {}", outpath.display());
+            fs::create_dir_all(&outpath)?;
+        } else {
+            println!(
+                "File: {} ({} bytes)",
+                outpath.display(),
+                file.size()
+            );
+
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(&p)?;
+                }
+            }
+
+            let mut outfile = fs::File::create(&outpath)?;
+            io::copy(&mut file, &mut outfile)?;
+        }
+
+        #[cfg(unix)]
+        {
+            use std::os::unix::fs::PermissionsExt;
+            if let Some(mode) = file.unix_mode() {
+                fs::set_permissions(&outpath, fs::Permissions::from_mode(mode))?;
+            }
+        }
+    }
+
+    Ok(())
+}
